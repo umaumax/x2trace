629cdd16d5f84e71117a7418f57d53adfd750749
umaumax <umauma7083@gmail.com>
2021-06-29 22:13:00 +0900

    Add filter tool

--- tools/README.md
+++ tools/README.md
@@ -1,12 +1,19 @@
 # tools
 
 ## how to setup
+for outlier.py
 ``` bash
 pip3 install rich
 pip3 install numpy
 ```
 
 ## how to run
+### outlier.py
 ``` bash
 ./outlier.py chrome-tracing.json
+```
+
+### filter.py
+``` bash
+./filter.py -b 2000 -e 3000 chrome-tracing.json
 ```
---
+++ tools/filter.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+import json
+import re
+import argparse
+from operator import itemgetter
+
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-b',
+                    '--begin-timestamp',
+                    help='begin timestamp[ms]',
+                    default=0,
+                    type=float)
+parser.add_argument('-e',
+                    '--end-timestamp',
+                    help='end timestamp[ms]',
+                    default=1000,
+                    type=float)
+parser.add_argument('--include',
+                    '--in',
+                    help='include function name regex pattern',
+                    default='^')
+parser.add_argument('--exclude',
+                    '--ex',
+                    help='exclude function name regex pattern',
+                    default='$^')
+parser.add_argument('input')
+parser.add_argument('args', nargs='*')
+
+args, extra_args = parser.parse_known_args()
+
+
+def main():
+    json_file = open(args.input, 'r')
+    json_root = json.load(json_file)
+
+    func_map = {}
+    index = 0
+    base_timestamp = 0
+    for v in json_root:
+        name = v['name']
+        if name not in func_map:
+            func_map[name] = {'stack': [], 'list': []}
+        ph = v['ph']
+        ts = v['ts']
+        if index == 0:
+            base_timestamp = ts
+        stack = func_map[name]['stack']
+        if ph == 'B':
+            stack.append((index, ts))
+        if ph == 'E':
+            (begin_index, begin_ts) = stack.pop()
+            end_index = index
+            end_ts = ts
+            func_map[name]['list'].append(
+                (begin_index, end_index, begin_ts, end_ts))
+        index += 1
+
+    interest_begin_ts = args.begin_timestamp * 1000.0 + base_timestamp
+    interest_end_ts = args.end_timestamp * 1000.0 + base_timestamp
+
+    include_pattern = re.compile(args.include)
+    exclude_pattern = re.compile(args.exclude)
+
+    valid_index_list = []
+    for func_name in func_map.keys():
+        result = include_pattern.match(func_name)
+        if result is None:
+            continue
+        result = exclude_pattern.match(func_name)
+        if result is not None:
+            continue
+        for info in func_map[func_name]['list']:
+            (begin_index, end_index, begin_ts, end_ts) = info
+            if begin_ts < interest_end_ts and interest_begin_ts < end_ts:
+                valid_index_list += [begin_index, end_index]
+
+    valid_index_list.sort()
+
+    filtered_json_data = itemgetter(*valid_index_list)(json_root)
+    print(json.dumps(list(filtered_json_data)))
+
+
+if __name__ == "__main__":
+    main()
